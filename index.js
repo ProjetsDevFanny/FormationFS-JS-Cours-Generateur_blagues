920b53a5cab9f529d4d8fc5b0f515668abd67a2f
Fanny Simon <contact.fanny.simon@gmail.com>
2025-03-28 13:05:54 +0100

    Condition affichage text/text_hidden, transition CSS

--- index.js
+++ index.js
@@ -15,10 +15,29 @@ async function fetchJokes() {
     .then((res) => res.json()) // On transforme la réponse en JSON
 
     .then((data) => {
+      console.log(data);
       console.log(data.data.content.text_head);
-      jokesContainer.innerHTML = `<h2>${data.data.content.text_head}</h2> 
-    <p>${data.data.content.text_hidden}</p>`;
+      console.log(data.data.content.text);
+      console.log(data.data.content.text_hidden);
+
+      if (data.data.content.text !== "") {
+        jokesContainer.innerHTML = `<h2>${data.data.content.text_head}</h2> 
+       <br><p>${data.data.content.text}</p>`;
+      } else {
+        jokesContainer.innerHTML = `<h2>${data.data.content.text_head}</h2> 
+       <br><p>${data.data.content.text_hidden}</p>`;
+      }
     });
 }
 
+// Charger une première blague au chargement de la page
 fetchJokes();
+
+// Changer la blague au clic sur la div
+jokesContainer.addEventListener("click", () => {
+  jokesContainer.classList.add("fade"); // Réduit l'opacité avant de changer la blague
+  setTimeout(() => {
+    fetchJokes();
+    jokesContainer.classList.remove("fade"); // Rétablit l'opacité après le changement
+  }, 300);
+});
